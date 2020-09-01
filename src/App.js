c033528804acdffc3425e433e28bb4d7fdc8e5f5
Cynthia Trujillo <developercynthia@gmail.com>
2020-08-31 21:15:35 -0500

    updated webp src in app.js, added alts, updated legend with credits

--- src/App.js
+++ src/App.js
@@ -12,20 +12,20 @@ function App() {
 <div style={styles}>
 <Carousel>
 <div>
-<img src="src/images/image1.webp" alt="Hong Kong" />
-<p className="legend">Hong Kong</p>
+<img src="https://raw.githubusercontent.com/publishct/carousel/master/src/images/image1.webp" alt="A team meeting with a group of co-workers who are smiling and have their arms all placed on the table to encourage team spirit" />
+<p className="legend">Photos: CC BY 3.0 US Mapbox Uncharted ERG</p>
 </div>
 <div>
-<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" alt="Singapore"/>
-<p className="legend">Singapore</p>
+<img src="https://raw.githubusercontent.com/publishct/carousel/master/src/images/image2.webp" alt="Four coworkers stand in a line facing the camera and they all are looking at the camera"/>
+<p className="legend">Photos: CC BY 3.0 US Mapbox Uncharted ERG</p>
 </div>
 <div>
-<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" alt="Japan"/>
-<p className="legend">Japan</p>
+<img src="https://raw.githubusercontent.com/publishct/carousel/master/src/images/image3.webp" alt="A close-up shot of a team fist bump with 3 peoples' hands touching. One person has arm tattoos. Another wears a few rings."/>
+<p className="legend">Photos: CC BY 3.0 US Mapbox Uncharted ERG</p>
 </div>
 <div>
-<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="Las Vegas"/>
-<p className="legend">Las Vegas</p>
+<img src="https://raw.githubusercontent.com/publishct/carousel/master/src/images/image4.webp" alt="A board room meeting with a diverse group of people and seemingly inclusive of many opinions"/>
+<p className="legend">Photos: CC BY 3.0 US Mapbox Uncharted ERG</p>
 </div>
 </Carousel>
 </div>
