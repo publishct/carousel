7834182af6adeacd81a278456d7df1330d8eeddc
Cynthia Trujillo <developercynthia@gmail.com>
2020-09-17 12:11:08 -0500

    Updates

---
+++ precache-manifest.f374dd94f0a8b7601cc14d4efffc3784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c286350aae1aafaa961695c91e01c36e",
+    "url": "/carousel/index.html"
+  },
+  {
+    "revision": "91ef500d565c1ca9af0b",
+    "url": "/carousel/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "e506814dfae76875a37e",
+    "url": "/carousel/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "91ef500d565c1ca9af0b",
+    "url": "/carousel/static/js/2.93e32125.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/carousel/static/js/2.93e32125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e506814dfae76875a37e",
+    "url": "/carousel/static/js/main.c180bb2d.chunk.js"
+  },
+  {
+    "revision": "2334337016e041c9c1cf",
+    "url": "/carousel/static/js/runtime-main.9b8b9a58.js"
+  }
+]);
